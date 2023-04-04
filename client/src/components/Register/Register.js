8fbcf8c1f4d87ee23df10e9da17af276ae4cd85c
Petar-Dinev <p.dinev55@gmail.com>
2023-04-04 22:55:58 +0300

    Add functionality to Register page

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { register } from "../../services/userService";
 import styles from "./registerCSS.css";
 
-export const Register = ({userData, createUser, onUserDataChange}) => {
+const Register = () => {
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const navigate = useNavigate();
+
+  const onUserDataChange = (e) => {
+    setUserData(state => ({...state, [e.target.name]: e.target.value}))
+  }
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault()
+    const {confirmPassword, ...userInfo} = userData
+    if(userData.password === userData.confirmPassword) {
+      const user = await register({...userInfo})
+       console.log(user);
+    } else {
+      alert('passwords missmatch')
+    }
+    
+    navigate('/login')
+  }
+
   return (
-    <form action="/register" method="post" style={styles} onSubmit={createUser}>
+    <form action="/register" method="post" style={styles} onSubmit={onSubmitHandler}>
       <label htmlFor="username">Username:</label>
       <input type="text" id="username" name="username" value={userData.username} onChange={onUserDataChange} required />
 
@@ -12,11 +41,13 @@ export const Register = ({userData, createUser, onUserDataChange}) => {
       <label htmlFor="password">Password:</label>
       <input type="password" id="password" name="password" value={userData.password} onChange={onUserDataChange} required />
 
-      <label htmlFor="confirm_password">Confirm Password:</label>
+      <label htmlFor="confirmPassword">Confirm Password:</label>
       <input
         type="password"
-        id="confirm_password"
-        name="confirm_password"
+        id="confirmPassword"
+        name="confirmPassword"
+        value={userData.confirmPassword}
+        onChange={onUserDataChange}
         required
       />
 
@@ -24,3 +55,5 @@ export const Register = ({userData, createUser, onUserDataChange}) => {
     </form>
   );
 };
+
+export default Register;
