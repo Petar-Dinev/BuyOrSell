ca62462251bf7dd271734e7c04d0c01b3ce1c397
Petar-Dinev <p.dinev55@gmail.com>
2023-04-04 22:54:53 +0300

    FIx paths to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,40 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import { Catalog } from "./components/Catalog/Catalog";
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Login from './components/Login/Login';
-import { Register } from './components/Register/Register';
-import styles from './app.css';
-import { Home } from './components/Home/Home';
+import Cars from "./components/Cars/Cars";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+import AuthProvider from "./contexts/AuthContext";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Logout from "./components/Logout/Logout";
+import CreateCarAd from "./components/Cars/CreateCarAd";
+import CarDetails from "./components/Cars/CarDetails";
+import EditCarAd from "./components/Cars/EditCarAd";
 
 function App() {
+
   return (
-    <div style={styles}>
-       <Header />
-       <main id="main-content">
-        <Routes>
-           <Route path='/login' element={<Login />} />
-           <Route path='/catalog' element={<Catalog />} />
-           <Route path='/register' element={<Register />} />
-           <Route path='/' element={<Home />} />
-        </Routes>
-       </main>
-       <Footer />
-    </div>
+    <AuthProvider>
+      <div>
+        <Header />
+        <main id="main-content">
+          <Routes>
+            <Route path="*" element={<PageNotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/cars" element={<Cars />} />
+            <Route path="/createCarAd" element={<CreateCarAd />} />
+            <Route path="/cars/:carId" element={<CarDetails />} />
+            <Route path="/cars/:carId/edit" element={<EditCarAd />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
 
