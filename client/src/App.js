f9db9b21144d95a0a939b9ffa96970f9b286cd10
Petar-Dinev <p.dinev55@gmail.com>
2023-04-08 12:05:24 +0300

    Fix some routes in app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,55 @@
 import { Route, Routes } from "react-router-dom";
 
-import Cars from "./components/CarsList/CarsList";
-import Footer from "./components/Footer/Footer";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CarAdProvider } from "./contexts/CarAdContext";
+
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import Home from "./components/Home/Home";
-import AuthProvider from "./contexts/AuthContext";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
 import Logout from "./components/Logout/Logout";
-import CreateCarAd from "./components/CarsList/CreateCarAd";
-import CarDetails from "./components/CarsList/CarDetails";
-import EditCarAd from "./components/CarsList/EditCarAd";
+import CreateCarAd from "./components/CreateCarAd/CreateCarAd";
+import CarDetails from "./components/CarDetails/CarDetails";
+import EditCarAd from "./components/EditCarAd/EditCarAd";
+import NotFound from "./components/NotFound/NotFound";
+import RouteGuard from "./components/Guards/RouteGuard";
+import OwnerGuard from "./components/Guards/OwnerGuard";
+import Catalog from "./components/Catalog/Catalog";
+
 
 function App() {
-
   return (
     <AuthProvider>
+      <CarAdProvider>
+    
       <div>
         <Header />
-        <main id="main-content">
-          <Routes>
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/cars" element={<CarsList />} />
-            <Route path="/createCarAd" element={<CreateCarAd />} />
-            <Route path="/cars/:carId" element={<CarDetails />} />
-            <Route path="/cars/:carId/edit" element={<EditCarAd />} />
+
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:carAdId" element={<CarDetails />} />
+          <Route element={<RouteGuard />}>
+            <Route path="/catalog/createAd" element={<CreateCarAd />} />
+            <Route
+              path="/catalog/:carAdId/edit"
+              element={
+                <OwnerGuard>
+                  <EditCarAd />
+                </OwnerGuard>
+              }
+            />
             <Route path="/logout" element={<Logout />} />
-          </Routes>
-        </main>
+          </Route>
+        </Routes>
+
         <Footer />
       </div>
+      </CarAdProvider>
     </AuthProvider>
   );
 }
