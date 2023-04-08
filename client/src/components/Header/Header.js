c2ea373febf540a19d3cc565025e2f6a0f44849e
Petar-Dinev <p.dinev55@gmail.com>
2023-04-08 12:04:01 +0300

    Fix header links

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,44 @@
-import {Link} from 'react-router-dom'
-import styles from './Header.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 function Header() {
+  const { isAuthenticated, userData } = useAuthContext();
+
   return (
     <header style={styles}>
-      <h1><Link to="/">CarsTrade</Link></h1>
+      <h1>
+        <Link to="/">CarsTrade</Link>
+      </h1>
       <nav>
         <ul>
-          <li><Link to="/cars">Catalog</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/about">About Us</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
+          <li>
+            <Link to="/catalog">Catalog</Link>
+          </li>
+          {isAuthenticated && (
+            <>
+              <span style={{color: "red","marginLeft": "1rem"}}>Hello {userData.username}</span>
+              <li>
+                <Link to="/catalog/createAd">Create Ad</Link>
+              </li>
+              <li>
+                <Link to="/logout">Logout</Link>
+              </li>
+            </>
+          )}
+          {!isAuthenticated && (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
         </ul>
       </nav>
     </header>
