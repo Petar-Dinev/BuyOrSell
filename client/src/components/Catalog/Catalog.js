072a98abce14d4b555665881b61438ef5cadaf8e
Petar-Dinev <p.dinev55@gmail.com>
2023-04-08 12:04:37 +0300

    Add catalog page

---
+++ client/src/components/Catalog/Catalog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Catalog.module.css';
+import { useCarAdContext } from '../../contexts/CarAdContext';
+import CarAdItem from './CarAdItem/CarAdItem';
+
+const Catalog = () => {
+    const {carAds} = useCarAdContext()
+
+  return (
+    <div className={styles.catalog}>
+      <h1>Catalog</h1>
+      <ul className={styles.products}>
+        {carAds.map((carAd) => (
+          <li key={carAd._id} className={styles.product}>
+            <CarAdItem carAd={carAd} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Catalog;
