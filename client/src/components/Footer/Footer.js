933576243f72651a3dbb9ae1ffd1caeab17f9047
Petar-Dinev <p.dinev55@gmail.com>
2023-04-08 12:06:59 +0300

    Rework footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,20 @@
-function Footer() {
+import React from 'react';
+import styles from './Footer.module.css';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
   return (
-    <footer>
-      <p>&copy; 2023 Petar Dinev</p>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>Copyright &copy; 2023</p>
+        <ul className={styles.nav}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contactUs">Contact Us</Link></li>
+        </ul>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
